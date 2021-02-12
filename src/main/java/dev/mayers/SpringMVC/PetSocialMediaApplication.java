4116ab0f873db149a87d02795ba8e32b9ee10359
Noah Mayers <NoahGCM@gmail.com>
2021-02-12 16:27:55 -0600

    added spring project

---
+++ src/main/java/dev/mayers/SpringMVC/PetSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package dev.mayers.SpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mayers/SpringMVC/PetSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mayers.SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
