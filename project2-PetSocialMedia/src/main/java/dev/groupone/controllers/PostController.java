87ed38362f57fe8afa84556a816c4350b8cb4705
Reese Poche <reesepoche@gmail.com>
2021-02-15 19:29:19 -0600

    added methods for post controller

--- project2-PetSocialMedia/src/main/java/dev/groupone/controllers/PostController.java
+++ project2-PetSocialMedia/src/main/java/dev/groupone/controllers/PostController.java
@@ -1,8 +1,125 @@
 package dev.groupone.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import dev.groupone.beans.Post;
+
 
 @RestController
 public class PostController {
+	
+	/**
+	 * gets all posts in DB in a list
+	 * @return
+	 */
+	@GetMapping(value = "/posts", produces = "application/json")
+	public List<Post> getAllPosts() {
+		return null;
+	}
+	
+	/**
+	 * Create a post. the poster will be the logged in user
+	 * @param newPost
+	 * @return
+	 */
+	@PostMapping(value = "/posts", consumes = "application/json", produces = "application/json")
+	public Post createPost(@RequestBody Post newPost) {
+		//creates the user with the given parameters 
+		
+		return null;
+	}
+	
+	
+	
+	
+	/**
+	 * gets post with the given id
+	 * @param id
+	 * @return
+	 */
+	@GetMapping(value = "/posts/{id}", produces = "application/json")
+	public Post getPost(@PathVariable("id") String id) {
+		//gets the user with the given id
+		return null;
+	}
+	
+	/**
+	 * edits or updates the post with the given id to the values given in the updatedPost in the put body
+	 * @param id
+	 * @param updatedPost
+	 * @return
+	 */
+	@PutMapping(value = "/posts/{id}", consumes = "application/json", produces = "application/json")
+	public Post updatePost( @PathVariable("id") int id, @RequestBody Post updatedPost) {
+		
+		
+		return null;
+	}
+	
+	/**
+	 * deletes the post with the given id in the path
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping(value = "/posts/{id}")
+	public boolean deletePost(@PathVariable("id") int id) {
+		System.out.println("Executing Delete");
+		
+		return false;
+	}
+	
+	/**
+	 * Logged in user likes the post in the body / id given in path
+	 * @param likedPost
+	 * @return
+	 */
+	@PostMapping(value = "/posts/{id}/like", consumes = "application/json", produces = "application/json")
+	public Post likePost(@RequestBody Post likedPost) {
+		//creates the user with the given parameters 
+		
+		return null;
+	}
+	
+	/**
+	 * Logged in user unlikes the post in the body / id given in the path
+	 * @param likedPost
+	 * @return
+	 */
+	@DeleteMapping(value = "/posts/{id}/like", consumes = "application/json", produces = "application/json")
+	public Post unlikePost(@RequestBody Post likedPost) {
+		//creates the user with the given parameters 
+		
+		return null;
+	}
+	
+	
+	
+	
+	/**
+	 * a search get request change variables when we get the variables we wnat to search by 
+	 * @param name
+	 * @param tag
+	 * @param ownerName
+	 * @param ownerId
+	 * @return
+	 */
+	@GetMapping(value = "/posts/search", produces = "application/json")
+	public List<Post> getUserByUsername(@RequestParam(required = false) String name, @RequestParam(required = false) String tag, 
+										@RequestParam(required = false) String ownerName, @RequestParam(required = false) String ownerId ) {
+		//returns the user with the given username
+		return null;
+	}
+	
+	
 
 }
