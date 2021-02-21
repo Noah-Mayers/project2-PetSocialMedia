5f91cc182954a29d02ba047ff3213cc1bfe005f4
Reese Poche <reesepoche@gmail.com>
2021-02-20 21:43:02 -0600

    finished comment controller

--- project2-PetSocialMedia/src/main/java/dev/groupone/controllers/CommentController.java
+++ project2-PetSocialMedia/src/main/java/dev/groupone/controllers/CommentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,9 +53,17 @@ public class CommentController {
 	 * @return
 	 */
 	@GetMapping(value = "/comments", produces = "application/json")
-	public List<Comment> getPostsComments(@PathVariable("id") String id) {
+	public List<Comment> getAllComments(@PathVariable("id") String id) {
 		//gets the user with the given id
 		return cs.getAllComment();
+	}
+	
+	
+	@GetMapping(value = "/comments/post/{id}", produces = "application/json")
+	public List<Comment> getPostsComments(@PathVariable("id") String id) {
+		//gets the user with the given id
+		Post post = ps.getPost(Integer.parseInt(id));
+		return cs.getAllCommentsForPost(post);
 	}
 	
 	
@@ -63,6 +72,7 @@ public class CommentController {
 	 * in the body, at least attach the correct post id to the comment
 	 * nothing else required will be overwritten anyway. 
 	 * @param newComment
+	 * @param id the id of the post the comment will be posted to
 	 * @return
 	 */
 	@PostMapping(value = "/comments", consumes = "application/json", produces = "application/json")
@@ -75,6 +85,16 @@ public class CommentController {
 		Post commentPost = ps.getPost(newComment.getPost().getId());
 		newComment.setPost(commentPost);
 		return cs.addComment(newComment);
+	}
+	
+	
+	
+	
+	@DeleteMapping(value = "/comments/{id}", consumes = "application/json", produces = "application/json")
+	public Comment deleteComment(@PathVariable("id") int id) {
+		Comment out = cs.getComment(id);
+		cs.deleteComment(id);
+		return out;
 	}
 	
 }
